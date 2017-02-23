9c5ea043c4e98acc4e883d93d0d94b2ae463d3a6
Ed Shadi <edshadi@gmail.com>
2017-02-23 15:31:05 -0800

    example app

--- example/src/app.jsx
+++ example/src/app.jsx
@@ -1,25 +1,101 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
 import FormFor from '../../src/form-for';
-const todo = {
-  name: 'Clean my room',
-  description: 'My room needs some serious cleaning',
-  completed: false,
-  list: 'Home',
-  position: 1,
+
+const TodoStore = {
+  all() {
+    return [{
+      id: 1, // this will be hidden field by default.
+      name: 'Clean my room',
+      description: 'My room needs some serious cleaning',
+      priority: 3,
+      createAt: '2014-10-09',
+      completed: true,
+      password: 'sweet todo',
+      list: 'Home',
+      email: 'myemail@gmail.com',
+      color: '#fc5803',
+    }];
+  },
+  new() {
+    return {
+      name: null,
+      description: null,
+      priority: null,
+      createAt: null,
+      completed: false,
+      password: null,
+      list: null,
+      email: null,
+      color: null,
+    };
+  },
+
+  find(index) {
+    return this.all()[index];
+  },
 };
 
-const inputOptions = {
-  labels: true, // by default we use placeholders, but you can turn on labels and we will use both.
-  description: { type: 'textarea' },
-  list: { type: 'select', values: [{value:"Home", label: "Home"}, {value:"Work", label: "Work"}] },
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: [],
+      alert: undefined,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(data) {
+    // this is where you call an action, e.g. TodoActions.createTodo. For this example
+    // I will just simulate errors and success here.
+    if (data.name === "") return this.setState({errors: ["name can't be blank"]});
+    console.log(data);
+    return this.setState({alert: "Success!", errors: []});
+  }
+
+  handleCancel() {
+    console.log('cancelling');
+  }
+
+  render() {
+    const inputOptions = {
+      // Uncomment that if you want to display labels for the fields, it's off by default
+      labels: false,
+      description: { type: 'textarea' },
+      createAt: { type: 'date' },
+      list: {
+        type: 'select', values: [
+          { value: 'Home', label: 'Home' },
+          { value: 'Work', label: 'Work' },
+        ],
+      },
+      color: { type: 'color' },
+      due: { type: 'datetime' },
+    };
+    return (
+      <div>
+        <span>{this.state.alert}</span>
+        <h2>Create</h2>
+        <FormFor
+          object={TodoStore.new()}
+          inputOptions={inputOptions}
+          errors={this.state.errors}
+        />
+        <h2>Update</h2>
+        <FormFor
+          object={TodoStore.find(0)}
+          inputOptions={inputOptions}
+          errors={this.state.errors}
+          onSubmit={this.handleSubmit}
+          onCancel={this.handleCancel}
+        />
+      </div>
+    );
+  }
+}
+
 ReactDom.render(
-  <FormFor
-    object={todo}
-    inputOptions={inputOptions}
-    onSubmit={(data) => { console.log(data); }}
-    onCancel={(data) => { console.log(data); }}
-  />,
+  <App />,
   document.getElementById('app')
 );
